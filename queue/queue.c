fc8e3cb875df43835c9f4e4e23a238bb678e752e
Daniel <drodriguez2933@gmail.com>
2022-04-14 10:28:08 -0400

    Add functions to queue to search for data, and find data at the nth index.

--- queue/queue.c
+++ queue/queue.c
@@ -4,70 +4,92 @@
 #include <stdlib.h>
 
 struct queue_ {
-    circular_list_t * p_list;
+    circular_list_t * list;
     size_t size;
 };
 
 queue_t * queue_create(compare_f compare, destroy_f destroy)
 {
-    queue_t * p_queue = calloc(sizeof(*p_queue), 1);
+    queue_t * queue = calloc(sizeof(*queue), 1);
 
-    if (p_queue == NULL)
+    if (queue == NULL)
     {
         perror("Queue memeory allocation.");
     }
     else
     {
-        p_queue->p_list = circular_create(compare, destroy);
+        queue->list = circular_create(compare, destroy);
     }
 
-    return p_queue;
+    return queue;
 }
 
-void queue_destroy(queue_t * p_queue)
+void queue_destroy(queue_t * queue)
 {
-    if (p_queue != NULL && p_queue->p_list)
+    if (queue != NULL && queue->list)
     {
 
-        if (p_queue->p_list)
+        if (queue->list)
         {
-           circular_destroy(p_queue->p_list); 
+           circular_destroy(queue->list); 
         }
         
-        free(p_queue);
+        free(queue);
     }
 }
 
-size_t queue_enqueue(queue_t * p_queue, void * p_data)
+size_t queue_enqueue(queue_t * queue, void * data)
 {
 
-    if (p_queue && p_queue->p_list)
+    if (queue && queue->list)
     {
-        p_queue->size = circular_insert(p_queue->p_list, p_data, FRONT);
+        queue->size = circular_insert(queue->list, data, BACK);
     }
 
-    return p_queue->size;
+    return queue->size;
 }
 
-void * queue_dequeue(queue_t * p_queue)
+void * queue_dequeue(queue_t * queue)
 {
-    void * p_data = NULL;
+    void * data = NULL;
 
-    if (p_queue->p_list)
+    if (queue->list)
     {
-        p_data = circular_remove_at(p_queue->p_list, BACK);
+        data = circular_remove_at(queue->list, FRONT);
     }
 
-    if (p_data != NULL)
+    if (data != NULL)
     {
-        p_queue->size--;
+        queue->size--;
     }
 
-    return p_data;
+    return data;
 }
 
 size_t queue_get_size(queue_t * queue)
 {
     return queue->size;
+}
+
+void * queue_search(queue_t * queue, void * data)
+{
+    void * return_data = NULL;
+    if ((queue != NULL) && (queue->list != NULL) && (data != NULL))
+    {
+        return_data = circular_search(queue->list, data);
+    }
+    
+    return return_data;
+}
+
+void * queue_find_nth(queue_t * queue, size_t idx)
+{
+    void * return_data = NULL;
+    if ((queue != NULL) && (queue->list != NULL) && (idx < queue->size))
+    {
+        return_data = circular_get_data(queue->list, idx);
+    }
+
+    return return_data;
 }
 // END OF SOURCE
--- queue/queue.h
+++ queue/queue.h
@@ -11,5 +11,7 @@ void queue_destroy(queue_t * p_queue);
 size_t queue_enqueue(queue_t * p_queue, void * p_data);
 void * queue_dequeue(queue_t * p_queue);
 size_t queue_get_size(queue_t * queue);
+void * queue_search(queue_t * queue, void * data);
+void * queue_find_nth(queue_t * queue, size_t idx);
 
 #endif
